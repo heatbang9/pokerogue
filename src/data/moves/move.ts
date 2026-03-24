8cd974b31b28975e80b0bff2af604bc965b533e2
User <user@Users-MacBook-Air.local>
2026-03-24 13:56:07 +0900

    fix: Add fail message for Helping Hand in single battles (Issue #6641)
    - Added fail message parameter (3) to failIfSingleBattle condition
    - Wild Pokemon using Helping Hand in single battles will now show appropriate fail message

--- src/data/moves/move.ts
+++ src/data/moves/move.ts
@@ -10196,7 +10196,7 @@ export function initMoves() {
       .attr(AddBattlerTagAttr, BattlerTagType.HELPING_HAND)
       .ignoresSubstitute()
       .target(MoveTarget.NEAR_ALLY)
-      .condition(failIfSingleBattle)
+      .condition(failIfSingleBattle, 3)
       // should stack multiplicatively if used multiple times in 1 turn
       .edgeCase(),
     new StatusMove(MoveId.TRICK, PokemonType.PSYCHIC, 100, 10, -1, 0, 3) //
